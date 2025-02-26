8113ab7fb1399ed4722f06c25964358346328f50
POA <33550681+poa@users.noreply.github.com>
2025-02-26 10:49:57 +0000

    Remove duplicated rpc requests to get latest block number

--- apps/explorer/lib/explorer/application.ex
+++ apps/explorer/lib/explorer/application.ex
@@ -158,6 +158,13 @@ defmodule Explorer.Application do
         configure_mode_dependent_process(Explorer.Migrator.ArbitrumDaRecordsNormalization, :indexer),
         configure_mode_dependent_process(Explorer.Migrator.ShrinkInternalTransactions, :indexer),
         configure_chain_type_dependent_process(Explorer.Chain.Cache.BlackfortValidatorsCounters, :blackfort),
+        configure_chain_type_dependent_process(Explorer.Chain.Cache.LatestL1BlockNumber, [
+          :optimism,
+          :polygon_edge,
+          :polygon_zkevm,
+          :scroll,
+          :shibarium
+        ]),
         configure_chain_type_dependent_process(Explorer.Chain.Cache.StabilityValidatorsCounters, :stability),
         configure_chain_type_dependent_con_cache(),
         Explorer.Migrator.SanitizeDuplicatedLogIndexLogs
